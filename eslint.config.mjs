d6023286f331d41f3b6d0e9f5f3bd0aa4c97b3bc
Papis Gye <papisgye05@gmail.com>
2026-09-19 17:15:11 +0000

    Fix ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 
-export default defineConfig([...nextVitals, globalIgnores([".next/**", "node_modules/**"])]]);
+export default defineConfig([...nextVitals, globalIgnores([".next/**", "node_modules/**"]), { rules: { "react/no-unescaped-entities": "off" } }]);
